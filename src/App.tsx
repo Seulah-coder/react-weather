4ba674c4bc8385a874f31bbe4ff9a69a930f1571
Seulah-coder <rser@hanmail.net>
2021-09-02 19:29:26 +0900

    weather test

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import './App.css';
+import { Weather } from './Weather';
+import WeatherInfo from './WeatherInfo';
 
-function App() {
+const has = (value:any): value is boolean => !!value;
+
+const App: React.FC = () => {
+
+  const [city, setCity] = useState<string>('London');
+
+  const handleChange = (event:ChangeEvent<HTMLInputElement>): void => {
+    setCity(event.target.value);
+  }
+
+  const handleSubmit = (event:FormEvent) => {
+    event.preventDefault();
+    getWeather(city);
+  };
+
+
+  const appId = '00f5969442f4b2204992782fb599d503';
+  const baseUrl ='https://api.openweathermap.org/data/2.5/weather?q=';
+  const suffix = '&units=metric&appid='+appId;
+
+  const [weather, setWeather] = useState<Weather | null>(null);
+
+  const getWeather = async (city: string): Promise<void> => {
+    const response = await fetch(baseUrl + city + suffix, {
+     
+    });
+    console.log(baseUrl + city + suffix);
+    if (response.status === 200) {
+      const jsonWeather = await response.json();
+      const cityTemp: Weather = jsonWeather.main;
+      cityTemp.city = jsonWeather.name;
+      setWeather(cityTemp);
+          
+      console.log(jsonWeather);
+    } else {
+      setWeather(null);
+    }
+  }
+
+  
+  useEffect(()=> {
+     getWeather(city);
+  }, []);
+
+  const [msgFromChild, setMsgFromChild] = useState('');
+  const getMsgFromChild = (msg:string) => setMsgFromChild(msg);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type ="text" placeholder ="Enter city" onInput = {handleChange}/>
+        <button type = "submit"> Get weather</button>
+        <h2>City: {city}</h2>
+       
+      </form>
+      {msgFromChild}
+      {has(weather) ? (
+        <WeatherInfo weather={weather} parentChannel = {getMsgFromChild}/>
+      ):(
+        <h2>No weather available</h2>
+      )}
     </div>
+
+    
+    </>
   );
 }
 
---
+++ src/WeatherInfo.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+import { Weather } from "./Weather";
+
+const WeatherInfo: React.FC<{weather: Weather, parentChannel:(msg:string) => void}> = ({weather, children, parentChannel}) => {
+    const{city, humidity, pressure, temp, temp_max, temp_min} = weather;
+    return(
+        <>
+            <div>
+                <h2>City:{city}</h2>
+                <h2>Temperature:{temp}</h2>
+                <h2>Max temperature:{temp_max}</h2>
+                <h2>Min temperature:{temp_min}</h2>
+                <h2>Humidity:{humidity}</h2>
+                <h2>Pressure:{pressure}</h2>
+
+            </div>
+
+            <div>
+                <button onClick={()=> parentChannel("Hello from child!")}>
+                    Say hello to parent
+                </button>
+            </div>
+        </>
+    );
+};
+
+export default WeatherInfo;
